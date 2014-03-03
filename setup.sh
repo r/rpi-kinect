85d73cb903d40f482562c9575188f972b548ee26
Raffi Krikorian <raffi.krikorian@gmail.com>
2014-03-03 13:05:18 -0800

    adding some comments

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
+# this is just my personal setup, and these are the two environment
+# variables that are needed by the Makefile
+
 export CCPREFIX=`pwd`/../tools/arm-bcm2708/arm-bcm2708-linux-gnueabi/bin/arm-bcm2708-linux-gnueabi-
 export KERNEL_SRC=`pwd`/../linux
